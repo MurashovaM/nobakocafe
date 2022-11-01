2a007e7e2f43f3636a8e73c1c3ce06925fac153f
MurashovaM <murashovam2016@google.com>
2022-11-01 12:53:23 +0200

    code refactoring

---
+++ js/script.js
@@ -0,0 +1,51 @@
+'use strict'
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android()
+			|| isMobile.BlackBerry()
+			|| isMobile.iOS()
+			|| isMobile.Opera()
+			|| isMobile.Windows()
+		);
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		menuArrows[0].addEventListener("click", function (e) {
+			menuArrows[0].parentElement.classList.toggle('_active');
+		});
+	}
+} else {
+	document.body.classList.add('_pc');
+}
+
+// МЕНЮ БУРГЕР
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
