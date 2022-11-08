9b9ec81b3f12fd1cb9c0dcf7a14b6067f14ef2a4
MurashovaM <murashovam2016@google.com>
2022-11-08 19:05:46 +0200

    popup

---
+++ js/tabs.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+const tabNavItems = document.querySelectorAll('.tabs-shop__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function(e){
+	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
+	if(targetElement.closest('.tabs-shop__button')){
+		tabNavItems.forEach((tabNavItem, index) => {
+			if(tabNavItem.classList.contains('active')){
+				currentActiveIndex = index;
+				tabNavItem.classList.remove('active');
+			}
+			if(tabNavItem === targetElement){
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add ('active');
+		tabItems[currentActiveIndex].classList.remove('active');
+		tabItems[newActiveIndex].classList.add('active');
+	}
+});
