b9b8dea238b9bd6cc35da43e92fe3a483898c51c
MurashovaM <murashovam2016@google.com>
2022-11-14 16:42:52 +0200

    commit

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -3,16 +3,16 @@
 
 		// *** SMTP *** //
 
-		 // require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
-		 // const HOST = '';
-		 // const LOGIN = '';
-		 // const PASS = '';
-		 // const PORT = '';
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		const HOST = 'smtp.gmail.com';
+		const LOGIN = 'murashovam2016@gmail.com';
+		const PASS = 'Welcome!995';
+		const PORT = '465';
 
 		// *** /SMTP *** //
    
-   const SENDER = 'sender@yandex.ru';
-   const CATCHER = 'catcher@ya.ru';
+   const SENDER = 'murashovam2016@gmail.com';
+   const CATCHER = 'murashovam2016@gmail.com';
    const SUBJECT = 'Заявка с сайта';
    const CHARSET = 'UTF-8';
-    
+   
