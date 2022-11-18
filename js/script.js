0173defe511cee17195a4149e0d9584c72747399
MurashovaM <murashovam2016@google.com>
2022-11-18 17:20:58 +0200

    delivery

--- js/script.js
+++ js/script.js
@@ -66,6 +66,6 @@ document.body.addEventListener('click', e => {
 	if (!e.target.matches('button')) return
 	document.querySelector('.popup__img img').src = e.target.dataset.src
 
-	document.querySelectorAll('button').forEach(btn => btn.classList.remove('active'))
-	e.target.classList.add('active')
+	document.querySelectorAll('button').forEach(btn => btn.classList.remove('changed'))
+	e.target.classList.add('changed')
 })
